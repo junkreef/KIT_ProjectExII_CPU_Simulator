428678847e05fe510e9ad9adaf1102b94c1e5b3a
Junpei Kishi <junpei0824@gmail.com>
2018-05-25 13:20:38 +0900

    Initial Commit
    Copy from moodle

---
+++ cpuboard.c
@@ -0,0 +1,24 @@
+/*
+ *	Project-based Learning II (CPU)
+ *
+ *	Program:	instruction set simulator of the Educational CPU Board
+ *	File Name:	cpuboard.c
+ *	Descrioption:	simulation(emulation) of an instruction
+ */
+
+#include	"cpuboard.h"
+
+
+/*=============================================================================
+ *   Simulation of a Single Instruction
+ *===========================================================================*/
+int
+step(Cpub *cpub)
+{
+
+
+
+	return RUN_HALT;
+}
+
+
